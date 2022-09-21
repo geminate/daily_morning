48512f31ae324f1a8ced42c009778320c8e56787
liuhuihao <liuhuihao@xdf.cn>
2022-09-21 16:18:01 +0800

    修改

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
@@ -7,7 +7,7 @@ import os
 import random
 from zhdate import ZhDate
 
-today = datetime.now()
+today = datetime.now() + timedelta(days = 1)
 start_date = os.environ['START_DATE']
 start_marry_date = os.environ['MARRY_START_DATE']
 city = os.environ['CITY']
@@ -56,17 +56,17 @@ def get_count2():
   return delta.days
 
 def get_birthday():
-  birth = ZhDate(date.today().year, int(birthday_m), int(birthday_d)).to_datetime()
-  if birth < datetime.strptime(datetime.now().strftime('%Y-%m-%d'),"%Y-%m-%d"):
-    birth = ZhDate(date.today().year + 1, int(birthday_m), int(birthday_d)).to_datetime()
+  birth = ZhDate(today.year, int(birthday_m), int(birthday_d)).to_datetime()
+  if birth < datetime.strptime(today.strftime('%Y-%m-%d'),"%Y-%m-%d"):
+    birth = ZhDate(today.year + 1, int(birthday_m), int(birthday_d)).to_datetime()
   diff = birth.toordinal() - today.toordinal()
   if diff == 0:
     return {"value":"今天是你的生日~ 生日快乐","color":"#FA9F4E"}
   return {"value":"距离你的生日还有 " + str(diff) + "天"}
 
 def get_birthday2():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.strptime(datetime.now().strftime('%Y-%m-%d'),"%Y-%m-%d"):
+  next = datetime.strptime(str(today.year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.strptime(today.strftime('%Y-%m-%d'),"%Y-%m-%d"):
     next = next.replace(year=next.year + 1)
   diff = next.toordinal() - today.toordinal()
   if diff == 0:
@@ -75,8 +75,8 @@ def get_birthday2():
 
 
 def get_marry_left():
-  next = datetime.strptime(str(date.today().year) + "-" + marry, "%Y-%m-%d")
-  if next < datetime.strptime(datetime.now().strftime('%Y-%m-%d'),"%Y-%m-%d"):
+  next = datetime.strptime(str(today.year) + "-" + marry, "%Y-%m-%d")
+  if next < datetime.strptime(today.strftime('%Y-%m-%d'),"%Y-%m-%d"):
     next = next.replace(year=next.year + 1)
   diff = next.toordinal() - today.toordinal()
   if diff == 0:
@@ -97,7 +97,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 weather_words, weather_words_color, weather, temp_low, temp_low_color, temp_high, temp_high_color = get_weather()
-data = {"today":{"value":datetime.now().strftime('%Y-%m-%d'),"color":"#87CEEB"},"weather_words":{"value":weather_words,"color":weather_words_color} ,"weather":{"value":weather},"temp_low":{"value":str(temp_low) + '℃',"color":temp_low_color},"temp_high":{"value":str(temp_high) + '℃',"color":temp_high_color},"next_words":{"value":"每一天都值得铭记↓","color":"#FFB6C1"},"next_words2":{"value":"余生有幸和你在一起","color":"#FFB6C1"},"love_days":{"value":get_count()},"marry_days":{"value":get_count2()},"birthday_left":get_birthday(),"birthday_left2":get_birthday2(),"marry_left":get_marry_left(),"words":{"value":get_words(), "color":get_random_color()}}
+data = {"today":{"value":today.strftime('%Y-%m-%d'),"color":"#87CEEB"},"weather_words":{"value":weather_words,"color":weather_words_color} ,"weather":{"value":weather},"temp_low":{"value":str(temp_low) + '℃',"color":temp_low_color},"temp_high":{"value":str(temp_high) + '℃',"color":temp_high_color},"next_words":{"value":"每一天都值得铭记↓","color":"#FFB6C1"},"next_words2":{"value":"余生有幸和你在一起","color":"#FFB6C1"},"love_days":{"value":get_count()},"marry_days":{"value":get_count2()},"birthday_left":get_birthday(),"birthday_left2":get_birthday2(),"marry_left":get_marry_left(),"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 res = wm.send_template(user_id2, template_id, data)
 print(res)
---
+++ test.py
@@ -0,0 +1,3 @@
+from datetime import date, datetime, timedelta
+today = (datetime.now() + timedelta(days = 1)).year
+print(today)

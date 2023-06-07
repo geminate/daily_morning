13b9ac9b5da0b2529c5045feb2f45404313a1253
liuhuihao <liuhuihao@xdf.cn>
2023-06-07 10:28:22 +0800

    修改

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import os
 import random
 from zhdate import ZhDate
+from dateutil.relativedelta import relativedelta
 
 today = datetime.now() + timedelta(days = 1)
 
@@ -16,6 +17,9 @@ weatherKey = os.environ['WEATHER_KEY']
 birthday_m = os.environ['BIRTHDAY_M']
 birthday_d = os.environ['BIRTHDAY_D']
 birthday = os.environ['BIRTHDAY']
+birthday_child = os.environ['BIRTHDAY_CHILD']
+birthday_child_date = os.environ['BIRTHDAY_CHILD_DATE']
+
 marry = os.environ['MARRY']
 last_day = os.environ['LAST_DAY']
 
@@ -60,6 +64,10 @@ def get_count2():
   delta = today - datetime.strptime(start_marry_date, "%Y-%m-%d")
   return  " 今天是我们结婚的 第"+str(delta.days)+"天 "
 
+def get_count3():
+  delta = today - datetime.strptime(birthday_child, "%Y-%m-%d")
+  return  " 今天是筱月出生的 第"+str(delta.days)+"天 "
+
 def get_birthday():
   birth = ZhDate(today.year, int(birthday_m), int(birthday_d)).to_datetime()
   if birth < datetime.strptime(today.strftime('%Y-%m-%d'),"%Y-%m-%d"):
@@ -78,6 +86,15 @@ def get_birthday2():
     return {"value":" 今天是小刘的生日~ 祝他生日快乐 ","color":"#FA9F4E"}
   return {"value":" 距离小刘的生日还有 " + str(diff) + "天 "}
 
+def get_birthday3():
+  next = datetime.strptime(str(today.year) + "-" + birthday_child_date, "%Y-%m-%d")
+  if next < datetime.strptime(today.strftime('%Y-%m-%d'),"%Y-%m-%d"):
+    next = next.replace(year=next.year + 1)
+  diff = next.toordinal() - today.toordinal()
+  if diff == 0:
+    return {"value":" 今天是筱月的生日~ 祝他生日快乐 ","color":"#FA9F4E"}
+  return {"value":" 距离筱月的生日还有 " + str(diff) + "天 "}
+
 
 def get_marry_left():
   next = datetime.strptime(str(today.year) + "-" + marry, "%Y-%m-%d")
@@ -88,18 +105,16 @@ def get_marry_left():
     return {"value":" 今天是我们两个的结婚纪念日~ 庆祝一下吧 ","color":"#FA9F4E"}
   return {"value":" 距离我们的结婚纪念日还有 " + str(diff) + "天 "}
 
-def get_child_left():
-  diff = (datetime.strptime(str(last_day), "%Y-%m-%d") + timedelta(days = 280)).toordinal() - today.toordinal() + 1
-  pro = round((280-diff) / 280 * 100,2)
-  return {"value":" 距宝宝出生还有 " + str(diff) + "天, 进度："+ str(pro) +"% "}
-
 def get_child_weeks():
-  ext = today.toordinal() - (datetime.strptime(str(last_day),"%Y-%m-%d") + timedelta(days = 1)).toordinal()
-  we = str(int( ext / 7))
-  da = str(ext % 7)
-  mm = str(int( ext / 30))
-  dam = str(ext % 30)
-  return {"value":" 宝宝已 " + str(we) + "周"+ str(da) +"天 / " + str(mm) + "月" + str(dam) + "天 " }
+  today = date.today()
+  birthdate_obj = datetime.strptime(str(birthday_child), '%Y-%m-%d').date()
+
+  rd = relativedelta(today, birthdate_obj)
+
+  years = rd.years
+  months = rd.months
+  days = rd.days
+  return {"value":" 筱月已 "+ str(years) +"岁"+ str(months) + "月" + str(days) + "天 " }
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -115,7 +130,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 weather_words, weather_words_color, weather, temp_low, temp_low_color, temp_high, temp_high_color = get_weather()
-data = {"today":{"value":" "+ str(today.strftime('%Y-%m-%d'))+ " ","color":"#87CEEB"},"weather_words":{"value":weather_words,"color":weather_words_color} ,"weather":{"value":weather},"temp_low":{"value":str(temp_low) + '℃',"color":temp_low_color},"temp_high":{"value":str(temp_high) + '℃',"color":temp_high_color},"next_words":{"value":"每一天都值得铭记↓","color":"#FFB6C1"},"next_words2":{"value":"余生有幸和你在一起","color":"#FFB6C1"},"love_days":{"value":get_count()},"marry_days":{"value":get_count2()},"birthday_left":get_birthday(),"birthday_left2":get_birthday2(),"marry_left":get_marry_left(),"child_left":get_child_left(),"child_weeks":get_child_weeks(),"words":{"value":get_words(), "color":get_random_color()}}
+data = {"today":{"value":" "+ str(today.strftime('%Y-%m-%d'))+ " ","color":"#87CEEB"},"weather_words":{"value":weather_words,"color":weather_words_color} ,"weather":{"value":weather},"temp_low":{"value":str(temp_low) + '℃',"color":temp_low_color},"temp_high":{"value":str(temp_high) + '℃',"color":temp_high_color},"next_words":{"value":"每一天都值得铭记↓","color":"#FFB6C1"},"next_words2":{"value":"余生有幸和你在一起","color":"#FFB6C1"},"love_days":{"value":get_count()},"marry_days":{"value":get_count2()},"born_days":{"value":get_count3()},"birthday_left":get_birthday(),"birthday_left2":get_birthday2(),"birthday_left3":get_birthday3(),"marry_left":get_marry_left(),"child_weeks":get_child_weeks(),"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 res = wm.send_template(user_id2, template_id, data)
 print(res)
